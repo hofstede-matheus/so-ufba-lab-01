21bb3eb6b1c69e787e248deb23235d932465662e
hofstede-matheus <matheus.hofstede@cubos.io>
2020-09-17 00:15:40 -0300

    implement argument checking and filename checking

--- src/filecopy.c
+++ src/filecopy.c
@@ -1,6 +1,35 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
-int main() {
-   // printf() displays the string inside quotation
-   printf("Hello, World!");
+
+#define EXPECTED_ARGUMENTS 3 /* program name + 2 arguments */
+
+void errorNoSuchAFileOrDirectory(char * filename) {
+    printf("filecopy: não foi possível abrir o arquivo %s: Arquivo ou diretório não existe. \n", filename);
+    exit(EXIT_FAILURE);
+}
+
+int main( int argc, const char ** argv ) {
+    char buf;
+    int sourcefile, destfile, n;
+
+    if (argc == EXPECTED_ARGUMENTS) {
+        printf("OK \n");
+
+        sourcefile = open(argv[1], O_RDONLY);
+
+        if (sourcefile == -1) {
+            errorNoSuchAFileOrDirectory(argv[1]);
+        }
+
+    } else {
+        printf("Número de argumentos inválidos \n");
+        exit (EXIT_FAILURE);
+    }
+
    return 0;
 }
+
